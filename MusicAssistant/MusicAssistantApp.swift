20d18a0b356f1271fedeaeb54ec67d3df853778a
Łukasz Michalak <lupinummail@gmail.com>
2024-08-31 01:19:07 +0200

    Initial Commit

---
+++ MusicAssistant/MusicAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAssistantApp.swift
+//  MusicAssistant
+//
+//  Created by Łukasz Michalak on 31/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
